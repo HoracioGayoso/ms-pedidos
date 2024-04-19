7ce14491f518f829cbef242d184f601c32c48174
hgayoso <hgayoso@folderit.net>
2024-04-19 20:07:48 -0300

    se comienza con el desarrollo

--- src/main/java/dan/ms/tp/mspedidos/controller/PedidoController.java
+++ src/main/java/dan/ms/tp/mspedidos/controller/PedidoController.java
@@ -1,14 +1,12 @@
 package dan.ms.tp.mspedidos.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import dan.ms.tp.mspedidos.modelo.EstadoPedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import dan.ms.tp.mspedidos.dao.PedidoRepository;
 import dan.ms.tp.mspedidos.modelo.Pedido;
@@ -28,4 +26,25 @@ public class PedidoController {
     public ResponseEntity<List<Pedido>> buscar(){
         return ResponseEntity.ok().body(repo.findAll());
     }
+
+    @GetMapping("/obtenerPorId")
+    public ResponseEntity<Optional<Pedido>> buscar(@RequestBody String pedidoId){
+        return ResponseEntity.ok().body(repo.findById(pedidoId));
+    }
+    @PutMapping("/cancelar")
+    public ResponseEntity<Pedido> cancelarPedido(@RequestBody String pedidoId){
+        Optional<Pedido> optionalPedido = repo.findById(pedidoId);
+        if (optionalPedido.isPresent()) {
+            Pedido pedido = optionalPedido.get();
+            // Realizar la lógica para cancelar el pedido aquí
+            // Por ejemplo, puedes cambiar el estado del pedido a "CANCELADO"
+            pedido.setEstado(EstadoPedido.CANCELADO); // ver como se settea un nuevo estado
+            // Guardar los cambios en la base de datos
+            pedido = repo.save(pedido);
+            return ResponseEntity.ok().body(pedido);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
--- src/main/java/dan/ms/tp/mspedidos/dao/PedidoRepository.java
+++ src/main/java/dan/ms/tp/mspedidos/dao/PedidoRepository.java
@@ -8,11 +8,12 @@ import dan.ms.tp.mspedidos.modelo.Pedido;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PedidoRepository extends MongoRepository<Pedido,String> {
     List<Pedido> findByClienteRazonSocial(String razonSocial);
-
+    Optional<Pedido> findById (String Id);
     @Query("{'cliente.razonSocial': ?0, 'fecha': {$gte: ?1, $lte: ?2}}")
     List<Pedido> findByClienteFecha(String razonSocial, Instant startDate, Instant endDate);
 }
